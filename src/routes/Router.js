30c886d7ea00908af21a37c479665f3df02fa640
arsalan414 <arsalanbaba414@yahoo.com>
2023-05-25 00:00:01 +0500

    added protected routr

--- src/routes/Router.js
+++ src/routes/Router.js
@@ -1,30 +1,22 @@
 import React from 'react';
-<<<<<<< HEAD
 import { BrowserRouter, Outlet, Route, Routes } from 'react-router-dom';
-=======
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
->>>>>>> origin/main
+
 
 //pages 
 import Singup from '../pages/Signup';
 import Login from '../pages/Login';
 import NotFound from '../pages/NotFound';
 import Home from '../pages/Home';
-<<<<<<< HEAD
 import PrivateRoutes from './PrivateRoutes';
 import Users from '../pages/Users';
 import Products from '../pages/Products';
-=======
-import NavBar from '../Components/NavBar';
->>>>>>> origin/main
+
 
 export default function Router(){
   return (
     <div>
         {/* <h1>hello world</h1> */}
         <BrowserRouter>
-<<<<<<< HEAD
-            
             <Routes>
                 <Route path='/' element={<Home />} >
                   <Route path="/signup" element={<Singup />} />
@@ -35,13 +27,7 @@ export default function Router(){
                   </Route>
 
                 </Route>
-=======
-            <NavBar />
-            <Routes>
-                <Route path='/' element={<Home />} />
-                <Route path="/signup" element={<Singup />} />
-                <Route path="/login" element={<Login />} />
->>>>>>> origin/main
+
                 <Route path="*" element={<NotFound />} />
             </Routes>
         </BrowserRouter>
