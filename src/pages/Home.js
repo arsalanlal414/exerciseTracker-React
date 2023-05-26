20af656c40f20814abebb6213c5e105062310884
yaseen-code <se.yaseen2@gmail.com>
2023-05-26 11:42:54 +0500

    added the front-end

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,21 @@
 import React from 'react'
-import { Outlet } from 'react-router-dom'
-
+import { Link, Outlet } from 'react-router-dom'
+// import { IoLogoStencil } from 'react-icons/io'
+import { RxStitchesLogo } from 'react-icons/io';
+import { RxFramerLogo } from 'react-icons/rx';
 import NavBar from '../Components/NavBar'
+import './home.scss'
 
 function Home() {
   return (
-    <div>
-      
+    <div className='home'>
+      <RxFramerLogo size="100px" className='Logo-icon'/>
+      <h1>WELCOME TO EXERCISE TRACKER APP</h1>
+      <p>Login with your login credential to continue</p>
+      <div>
+        <button><Link to="/login">Log in</Link></button>
+        <button><Link to="/signup">Sign up</Link></button>
+      </div>
     </div>
   )
 }
